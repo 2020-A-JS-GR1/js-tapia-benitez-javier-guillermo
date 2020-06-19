180dacb1e682324be79345415093a34eab1988a7
Javier Tapia <javier.tapia@epn.edu.ec>
2020-06-19 16:55:08 -0500

    Arreglos e intro operadores

--- 01-Javascript/02-arreglos.js
+++ 01-Javascript/02-arreglos.js
@@ -1,4 +1,4 @@
-const arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arreglo = [1, 2, 3, 4, 5, 8, 9, 10];
 // arreglo = 1;
 /*let cualquierCosa = "ASDF";
 cualquierCosa = 2;
@@ -9,3 +9,35 @@ cualquierCosa = {};
 cualquierCosa = [];
 console.log(cualquierCosa);*/
 const arregloTodo = [true, 1, 1.2, "Adrian", undefined, null, {}, [1, 2, true, "A"]];
+let a = [1,2,3];
+const b = Object.assign([],a);
+
+// for of
+for (let numero of arreglo){ // VALORES
+    console.log('numero', numero);
+}
+
+// for in
+for (let indice in arreglo){ // INDICES
+    arreglo[indice];
+    console.log('indice', indice);
+}
+
+arreglo.push(11); // Añadir al final un elemento
+console.log(arreglo);
+// [1, 2, 3, 4, 5, 8, 9, 10, 11]
+arreglo.pop(); // Eliminar al final un elemento
+console.log(arreglo);
+// [1, 2, 3, 4, 5, 8, 9, 10]
+arreglo.unshift(6); // Añadir al principio del rreglo
+console.log(arreglo);
+// [6, 1, 2, 3, 4, 5, 8, 9, 10]
+arreglo.splice(0,1);
+console.log(arreglo);
+arreglo.splice(0,0,3,4,5);
+console.log(arreglo);
+
+const indice = arreglo.indexOf(9);
+console.log('indice', indice); // 6
+arreglo.splice(arreglo.indexOf(9),1);
+console.log(arreglo);
