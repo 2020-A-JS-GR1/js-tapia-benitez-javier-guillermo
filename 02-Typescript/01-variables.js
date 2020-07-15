cfcd2447be7b575b19ab797ce0bebb726dfc982e
Javier Tapia <javier.tapia@epn.edu.ec>
2020-07-14 19:14:17 -0500

    Funciones y Clases TS

---
+++ 02-Typescript/01-variables.js
@@ -0,0 +1,17 @@
+let nombre = 'Adrian';
+// nombre = 1;
+nombre = 'Vicente';
+// Duck Typing
+let edad = 31;
+let casado = false;
+let fecha = new Date();
+let sueldo;
+sueldo = 12.4;
+let marihuana = 2;
+marihuana = '2';
+marihuana = true;
+marihuana = () => '2';
+let edadMultiple = 2;
+edadMultiple = '2';
+edadMultiple = 2222;
+edadMultiple = 'dos';
