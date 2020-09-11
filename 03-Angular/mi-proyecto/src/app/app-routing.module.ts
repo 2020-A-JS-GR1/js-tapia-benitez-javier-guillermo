516a188719db802ecf644acef6861fe9401218fc
Javier Tapia <javier.tapia@epn.edu.ec>
2020-09-11 18:00:46 -0500

    Rutas, Rutas hijos y Servicios

--- 03-Angular/mi-proyecto/src/app/app-routing.module.ts
+++ 03-Angular/mi-proyecto/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { RutaEditarUsuarioComponent } from './rutas/ruta-editar-usuario/ruta-editar-usuario.component';
+import { RutaCrearUsuarioComponent } from './rutas/ruta-crear-usuario/ruta-crear-usuario.component';
+import { RutaListaUsuarioComponent } from './rutas/ruta-lista-usuario/ruta-lista-usuario.component';
 import { RutaLoginComponent } from './rutas/ruta-login/ruta-login.component';
 import { RutaInicioComponent } from './rutas/ruta-inicio/ruta-inicio.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RutaUsuarioComponent } from './rutas/ruta-usuario/ruta-usuario.component';
 
 const routes: Routes = [
   {
@@ -11,6 +15,29 @@ const routes: Routes = [
   {
     component: RutaLoginComponent,
     path: 'login'
+  },
+  {
+    component: RutaUsuarioComponent,
+    path: 'usuario',
+    children: [
+      {
+        path: 'lista',
+        component: RutaListaUsuarioComponent
+      },
+      {
+        path: 'crear',
+        component: RutaCrearUsuarioComponent
+      },
+      {
+        path: 'editar/:id',
+        component: RutaEditarUsuarioComponent
+      },
+      {
+        path: '',
+        redirectTo: 'lista',
+        pathMatch: 'full'
+      }
+    ]
   },
   {
     path: '',
---
+++ 03-Angular/mi-proyecto/src/app/rutas/ruta-usuario/ruta-usuario.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ruta-usuario',
+  templateUrl: './ruta-usuario.component.html',
+  styleUrls: ['./ruta-usuario.component.css']
+})
+export class RutaUsuarioComponent implements OnInit {
+
+  arregloUsuarios = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
